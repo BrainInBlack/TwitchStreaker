16d479f328c948204a79215810045f19e714b21a
Daniel Lieberwirth <daniellieberwirth@gmail.com>
2019-08-16 05:43:42 +0200

    Upped Version Number

--- TwitchStreaker_StreamlabsSystem.py
+++ TwitchStreaker_StreamlabsSystem.py
@@ -21,7 +21,7 @@ from StreamlabsEventReceiver import StreamlabsEventClient
 ScriptName = "Twitch Streaker"
 Website = "https://github.com/BrainInBlack/TwitchStreaker"
 Creator = "BrainInBlack"
-Version = "2.0.0"
+Version = "2.0.1"
 Description = "Tracker for new and gifted subscriptions with a streak mechanic."
 
 # ----------------
